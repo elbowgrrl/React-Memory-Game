1e5c0dc22032f5b2b876588cb842d34a3c30f46f
Megan V <elbowgrrl@gmail.com>
2022-03-02 12:59:34 -0800

    complted game logic

--- src/App.js
+++ src/App.js
@@ -26,6 +26,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null)
+    setChoiceTwo(null)
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -69,6 +71,12 @@ function App() {
     }
   }, [choiceOne, choiceTwo]);
 
+  useEffect(() =>{
+    shuffleCards()
+  }, [])
+
+  
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -84,6 +92,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns Taken: {turns}</p>
     </div>
   );
 }
