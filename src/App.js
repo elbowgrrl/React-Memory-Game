31c835f19acc1cd0c44728c98a305c4e19b85fb8
Megan V <elbowgrrl@gmail.com>
2022-03-02 12:23:47 -0800

    adding css animation to cards

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
         });
         resetTurn();
       } else {
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000)
       }
     }
   }, [choiceOne, choiceTwo]);
